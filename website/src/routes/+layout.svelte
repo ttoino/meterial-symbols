c61627fcb5970544f5f3ee4aa3b084285a7618d1
toino <me@toino.pt>
2025-09-23 20:53:44 +0100

    Add title to website

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -4,6 +4,10 @@
     const { children } = $props();
 </script>
 
+<svelte:head>
+    <title>Meterial Symbols</title>
+</svelte:head>
+
 <main class="flex h-full w-full flex-col items-center justify-center gap-8 p-4">
     <h1
         class="text-center text-title-l medium:text-display-s large:text-display-l"
