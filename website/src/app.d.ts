6bdc375fadb88835a8382ff37b4ac14805863841
toino <me@toino.pt>
2026-03-05 22:59:15 +0000

    Migrate to cloudflare pages

--- website/src/app.d.ts
+++ website/src/app.d.ts
@@ -1,11 +1,14 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
     namespace App {
+        interface Platform {
+            cf: CfProperties;
+            ctx: ExecutionContext;
+            env: Env;
+        }
+
         // interface Error {}
         // interface Locals {}
         // interface PageData {}
-        // interface Platform {}
     }
 }
 
