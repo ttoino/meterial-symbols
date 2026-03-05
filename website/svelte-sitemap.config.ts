c56e3a5a1bc0e27526a1b12f72c45bfead0998e5
toino <me@toino.pt>
2026-03-05 23:38:54 +0000

    Add robots.txt and sitemap config

---
+++ website/svelte-sitemap.config.ts
@@ -0,0 +1,6 @@
+import type { OptionsSvelteSitemap } from "svelte-sitemap";
+
+export default {
+    domain: "https://meterial.toino.pt",
+    outDir: ".svelte-kit/cloudflare",
+} satisfies OptionsSvelteSitemap;
